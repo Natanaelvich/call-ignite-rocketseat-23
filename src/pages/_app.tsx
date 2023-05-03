ccfb754dc86aa97b6d060dbd777038b9be7fe482
Natanaelvich <taelima1997@gmail.com>
2023-05-02 23:13:24 -0300

    add config default project

---
+++ src/pages/_app.tsx
@@ -0,0 +1,11 @@
+import type { AppProps } from "next/app";
+import { globalStyles } from "../styles/global";
+
+globalStyles();
+
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps) {
+  return <Component {...pageProps} />;
+}
