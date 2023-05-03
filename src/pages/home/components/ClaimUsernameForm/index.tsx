c6f80dbadbe6e95d54bced196733b2578e531633
Natanaelvich <taelima1997@gmail.com>
2023-05-02 23:03:44 -0300

    add implementation for initi register user

--- src/pages/home/components/ClaimUsernameForm/index.tsx
+++ src/pages/home/components/ClaimUsernameForm/index.tsx
@@ -2,24 +2,61 @@ import { ArrowRight } from "phosphor-react";
 
 import { Form, FormAnnotation } from "./styles";
 import { TextInput, Button, Text } from "@natanaelvich-ignite-ui/react";
+import { useRouter } from "next/router";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+
+const ClaimUsernameFormSchema = z.object({
+  username: z
+    .string()
+    .min(3, { message: "O usuário precisa ter pelo menos 3 letras." })
+    .regex(/^([a-z\\-]+)$/i, {
+      message: "O usuário pode ter apenas letras e hifens.",
+    })
+    .transform((username) => username.toLowerCase()),
+});
+
+type ClaimUsernameFormData = z.infer<typeof ClaimUsernameFormSchema>;
 
 export function ClaimUsernameForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm<ClaimUsernameFormData>({
+    resolver: zodResolver(ClaimUsernameFormSchema),
+  });
+
+  const router = useRouter();
+
+  async function handleClaimUsername(data: ClaimUsernameFormData) {
+    const { username } = data;
+
+    router.push(`/register?username=${username}`);
+  }
 
   return (
     <>
-      <Form as="form">
+      <Form as="form" onSubmit={handleSubmit(handleClaimUsername)}>
         <TextInput
           prefix="call.com/"
           placeholder="seu-usuário"
+          {...register("username")}
         />
-        <Button  type="submit">
+        <Button type="submit" disabled={isSubmitting}>
           Reservar
           <ArrowRight />
         </Button>
       </Form>
 
       <FormAnnotation>
-        <Text size="sm">Digite o nome do usuário desejado</Text>
+        <Text size="sm">
+          {" "}
+          {errors.username
+            ? errors.username.message
+            : "Digite o nome do usuário desejado"}
+        </Text>
       </FormAnnotation>
     </>
   );
