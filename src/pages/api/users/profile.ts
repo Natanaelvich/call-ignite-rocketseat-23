1b8b663a95f3bd0aa0c3bd20f6440c805e31d60e
Natanaelvich <taelima1997@gmail.com>
2023-05-05 22:53:27 -0300

    add initial routes users

---
+++ src/pages/api/users/profile.ts
@@ -0,0 +1,12 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method !== "PUT") {
+    return res.status(405).end();
+  }
+
+  return res.status(204).end();
+}
---
+++ src/pages/api/users/time-intervals.ts
@@ -0,0 +1,13 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { z } from "zod";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method !== "POST") {
+    return res.status(405).end();
+  }
+
+  return res.status(201).end();
+}
