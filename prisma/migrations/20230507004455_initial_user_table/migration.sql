4517587eaef00003c2ef69c8f6be063ad35b27a5
Natanaelvich <taelima1997@gmail.com>
2023-05-06 21:48:43 -0300

    add initial migrations

---
+++ prisma/migrations/20230507004455_initial_user_table/migration.sql
@@ -0,0 +1,14 @@
+-- CreateTable
+CREATE TABLE `users` (
+    `id` VARCHAR(191) NOT NULL,
+    `username` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NOT NULL,
+    `bio` TEXT NULL,
+    `email` VARCHAR(191) NULL,
+    `avatar_url` VARCHAR(191) NULL,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    UNIQUE INDEX `users_username_key`(`username`),
+    UNIQUE INDEX `users_email_key`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
