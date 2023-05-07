186fa0b8e9a92b28a36e55499fa99b5fce4ccd2e
Natanaelvich <taelima1997@gmail.com>
2023-05-06 21:50:18 -0300

    add prisma adapter to google auth

---
+++ src/lib/auth/prisma-adapter.ts
@@ -0,0 +1,117 @@
+import { NextApiRequest, NextApiResponse, NextPageContext } from "next";
+import { Adapter } from "next-auth/adapters";
+import { parseCookies, destroyCookie } from "nookies";
+import { prisma } from "../prisma";
+
+export function PrismaAdapter(
+  req: NextApiRequest | NextPageContext["req"],
+  res: NextApiResponse | NextPageContext["res"]
+): Adapter {
+  return {
+    async createUser(user) {
+      const { "@ignitecall:userId": userIdOnCookies } = parseCookies({ req });
+
+      if (!userIdOnCookies) {
+        throw new Error("User ID not found on cookies.");
+      }
+
+      const prismaUser = await prisma.user.update({
+        where: {
+          id: userIdOnCookies,
+        },
+        data: {
+          name: user.name,
+          email: user.email,
+          avatar_url: user.avatar_url,
+        },
+      });
+
+      destroyCookie({ res }, "@ignitecall:userId", {
+        path: "/",
+      });
+
+      return {
+        id: prismaUser.id,
+        name: prismaUser.name,
+        username: prismaUser.username,
+        email: prismaUser.email!,
+        emailVerified: null,
+        avatar_url: prismaUser.avatar_url!,
+      };
+    },
+
+    async getUser(id) {
+      const user = await prisma.user.findUnique({
+        where: {
+          id,
+        },
+      });
+
+      if (!user) {
+        return null;
+      }
+
+      return {
+        id: user.id,
+        name: user.name,
+        username: user.username,
+        email: user.email!,
+        emailVerified: null,
+        avatar_url: user.avatar_url!,
+      };
+    },
+    async getUserByEmail(email) {
+      const user = await prisma.user.findUnique({
+        where: {
+          email,
+        },
+      });
+
+      if (!user) {
+        return null;
+      }
+
+      return {
+        id: user.id,
+        name: user.name,
+        username: user.username,
+        email: user.email!,
+        emailVerified: null,
+        avatar_url: user.avatar_url!,
+      };
+    },
+    async getUserByAccount({ providerAccountId, provider }) {},
+
+    async updateUser(user) {
+      const prismaUser = await prisma.user.update({
+        where: {
+          id: user.id!,
+        },
+        data: {
+          name: user.name,
+          email: user.email,
+          avatar_url: user.avatar_url,
+        },
+      });
+
+      return {
+        id: prismaUser.id,
+        name: prismaUser.name,
+        username: prismaUser.username,
+        email: prismaUser.email!,
+        emailVerified: null,
+        avatar_url: prismaUser.avatar_url!,
+      };
+    },
+
+    async linkAccount(account) {},
+
+    async createSession({ sessionToken, userId, expires }) {},
+
+    async getSessionAndUser(sessionToken) {},
+
+    async updateSession({ sessionToken, userId, expires }) {},
+
+    async deleteSession(sessionToken) {},
+  };
+}
