5f1deaa0a459b83516b4e5e7fd5ae4d09d28e85f
Natanaelvich <taelima1997@gmail.com>
2023-05-07 22:44:35 -0300

    add api routes time intervals

--- src/pages/api/users/time-intervals.ts
+++ src/pages/api/users/time-intervals.ts
@@ -1,13 +1,51 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import { z } from "zod";
+import { NextApiRequest, NextApiResponse } from 'next'
+import { z } from 'zod'
+import { prisma } from '../../../lib/prisma'
+import { getServerSession } from 'next-auth'
+import { buildNextAuthOptions } from '../auth/[...nextauth]'
+
+const timeIntervalsBodySchema = z.object({
+  intervals: z.array(
+    z.object({
+      weekDay: z.number(),
+      startTimeInMinutes: z.number(),
+      endTimeInMinutes: z.number(),
+    }),
+  ),
+})
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse
+  res: NextApiResponse,
 ) {
-  if (req.method !== "POST") {
-    return res.status(405).end();
+  if (req.method !== 'POST') {
+    return res.status(405).end()
   }
 
-  return res.status(201).end();
+  const session = await getServerSession(
+    req,
+    res,
+    buildNextAuthOptions(req, res),
+  )
+
+  if (!session) {
+    return res.status(401).end()
+  }
+
+  const { intervals } = timeIntervalsBodySchema.parse(req.body)
+
+  await Promise.all(
+    intervals.map((interval) => {
+      return prisma.userTimeInterval.create({
+        data: {
+          week_day: interval.weekDay,
+          time_start_in_minutes: interval.startTimeInMinutes,
+          time_end_in_minutes: interval.endTimeInMinutes,
+          user_id: session.user?.id,
+        },
+      })
+    }),
+  )
+
+  return res.status(201).end()
 }
