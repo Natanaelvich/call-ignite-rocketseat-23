42ca7a6b5fa2df5cd622edc04ec3ca9d2cefb49b
Natanaelvich <taelima1997@gmail.com>
2023-05-07 22:45:10 -0300

    update getStaticProps with session data

--- src/pages/schedule/[username]/index.tsx
+++ src/pages/schedule/[username]/index.tsx
@@ -2,6 +2,7 @@
 import { GetStaticPaths, GetStaticProps } from 'next'
 import { Container, UserHeader } from './styles'
 import { Avatar, Heading, Text } from '@natanaelvich-ignite-ui/react'
+import { prisma } from '../../../lib/prisma'
 
 interface ScheduleProps {
   user: {
@@ -28,35 +29,36 @@ export default function Schedule({ user }: ScheduleProps) {
 }
 
 export const getStaticPaths: GetStaticPaths = async () => {
-  return {
-    paths: [],
-    fallback: 'blocking',
+    return {
+      paths: [],
+      fallback: 'blocking',
+    }
   }
-}
-
-export const getStaticProps: GetStaticProps = async ({ params }) => {
-//   const username = String(params?.username)
-
-//   const user = await prisma.user.findUnique({
-//     where: {
-//       username,
-//     },
-//   })
-
-//   if (!user) {
-//     return {
-//       notFound: true,
-//     }
-//   }
-
-  return {
-    props: {
-      user: {
-        name: 'Natanael Vich',
-        bio: 'Desenvolvedor Fullstack',
-        avatarUrl: 'http://github.com/natanaelvich.png',
+  
+  export const getStaticProps: GetStaticProps = async ({ params }) => {
+    const username = String(params?.username)
+  
+    const user = await prisma.user.findUnique({
+      where: {
+        username,
       },
-    },
-    revalidate: 60 * 60 * 24, // 1 day
+    })
+  
+    if (!user) {
+      return {
+        notFound: true,
+      }
+    }
+  
+    return {
+      props: {
+        user: {
+          name: user.name,
+          bio: user.bio,
+          avatarUrl: user.avatar_url,
+        },
+      },
+      revalidate: 60 * 60 * 24, // 1 day
+    }
   }
-}
+  
