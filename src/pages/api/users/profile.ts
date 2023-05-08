c0a56016f1eb34e21004d4469ebc68b854fb2af8
Natanaelvich <taelima1997@gmail.com>
2023-05-07 22:44:21 -0300

    add api route put user

--- src/pages/api/users/profile.ts
+++ src/pages/api/users/profile.ts
@@ -1,4 +1,12 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import { getServerSession } from "next-auth";
+import { z } from "zod";
+import { prisma } from "../../../lib/prisma";
+import { buildNextAuthOptions } from "../auth/[...nextauth]";
+
+const updateProfileBodySchema = z.object({
+  bio: z.string(),
+});
 
 export default async function handler(
   req: NextApiRequest,
@@ -7,6 +15,27 @@ export default async function handler(
   if (req.method !== "PUT") {
     return res.status(405).end();
   }
+
+  const session = await getServerSession(
+    req,
+    res,
+    buildNextAuthOptions(req, res)
+  );
+
+  if (!session) {
+    return res.status(401).end();
+  }
+
+  const { bio } = updateProfileBodySchema.parse(req.body);
+
+  await prisma.user.update({
+    where: {
+      id: session.user.id,
+    },
+    data: {
+      bio,
+    },
+  });
 
   return res.status(204).end();
 }
